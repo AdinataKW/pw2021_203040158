cd52bfbb531c12ced79d532e7edd7ada2bf5ee9d
AdinataKW <adinata2862@gmail.com>
2021-02-12 19:00:45 +0700

    menyelesaikan video pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,80 @@
+<?php 
+/*
+Adinata Kusuma Wardani
+203040158
+https://github.com/AdinataKW/pw2021_203040158
+Pertemuan 2 - 12 Februari 2021
+Mempelajari mengenai sintaks PHP
+*/
+?>
+
+
+<?php
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+//  var_dump
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// tidak boleh di awali dengan angka, tapi boleh mengandung angka
+// $nama = "Adinata Kusuma Wardani";
+// echo "Halo, nama saya $nama";
+
+// Operator
+// Aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// Penggabung String / concatenation / concet
+// . 
+// $nama_depan = "Adinata";
+// $nama_belakang = "Kusuma Wardani";
+// echo $nama_depan . " ". $nama_belakang;
+
+// Assignment
+// =, +=, -+, *=, /=, %=, .=
+// $nama = "Adinata";
+// $nama .= " ";
+// $nama .= "Kusuma Wardani";
+// echo $nama;
+
+// Perbandingan
+//  <, >, <=, >=, == , 1=
+// var_dump(1 == "1")
+
+// Identitas 
+// // ===, 1==
+// var_dump(1 === "1")
+
+// Logika
+// &&, ||, !
+$x = 30;
+var_dump($x < 20 || $x % 2 == 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
